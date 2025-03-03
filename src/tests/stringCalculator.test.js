fe7408223e80f1f9bb072e0624ad8e481f3d35ed
draj812602 <draj.8126@gmail.com>
2025-03-03 23:04:05 +0530

    failed third test case

--- src/tests/stringCalculator.test.js
+++ src/tests/stringCalculator.test.js
@@ -8,4 +8,7 @@ describe("String Calculator", () => {
   test("should return the number itself when a single number is provided", () => {
     expect(add("1")).toBe(1);
   });
+  test("should return the sum of two numbers", () => {
+    expect(add("1,5")).toBe(6);
+  });
 });
