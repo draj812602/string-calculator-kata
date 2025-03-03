469b1c04654ae41d42087a66feb46b2a675de10a
draj812602 <draj.8126@gmail.com>
2025-03-03 23:03:17 +0530

    failed because our function only handles an empty string.

--- src/tests/stringCalculator.test.js
+++ src/tests/stringCalculator.test.js
@@ -5,4 +5,7 @@ describe("String Calculator", () => {
   test("should return 0 for an empty string", () => {
     expect(add("")).toBe(0);
   });
+  test("should return the number itself when a single number is provided", () => {
+    expect(add("1")).toBe(1);
+  });
 });
